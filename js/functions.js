953662f80ae30b7cb54600342b5cf114af243a33
pds85 <pablosanchez@MacBook-Pro-de-Pablo.local>
2017-01-08 12:50:10 -0300

    avanzo con todo el html del index y empresa

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+	$('.bxslider').bxSlider({
+		minSlides: 3,
+  		maxSlides: 4,
+  		slideWidth: 270,
+  		slideMargin: 10,
+  		infiniteLoop: true
+	});
+
+	$('.bxslider-header').bxSlider();
+});
